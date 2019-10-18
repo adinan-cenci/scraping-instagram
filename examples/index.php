131b123e0e66f06ff9a3132a5ae62293a41c1fd5
Adinan Cenci <adinancenci@protonmail.com>
2019-10-18 09:01:13 -0300

    6th commit

--- examples/index.php
+++ examples/index.php
@@ -4,25 +4,25 @@ require '../vendor/autoload.php';
 
 //---------------------------------------------
 
-$handle 	= 'pewdiepie';
-$scraper 	= new Instagram($handle);
+$handle     = 'pewdiepie';
+$scraper    = new Instagram($handle);
 
 //---------------------------------------------
 
 try {
-	$pictures = $scraper->fetch();
+    $pictures = $scraper->fetch();
 } catch (\Exception $e) {
-	echo 
-	'Error: '.$e->getMessage();
-	die();
+    echo 
+    'Error: '.$e->getMessage();
+    die();
 }
 
 //---------------------------------------------
 
 foreach ($pictures as $p) {
-	echo 
-	"<h2>{$p['caption']}</h2>
-	<a href=\"{$p['src']}\" target=\"_blank\" title=\"click to enlarge\"> 
-    	<img src=\"{$p['thumbnails']['150x150']}\" alt=\"{$p['caption']}\" /> 
+    echo 
+    "<h2>{$p['caption']}</h2>
+    <a href=\"{$p['src']}\" target=\"_blank\" title=\"click to enlarge\"> 
+        <img src=\"{$p['thumbnails']['150x150']}\" alt=\"{$p['caption']}\" /> 
     </a>";
 }
