4e4fd2f01cb98e60d5845c3d3c639346567965a2
Adinan Cenci <adinancenci@protonmail.com>
2020-02-08 08:24:18 -0300

    Update CHANGELOG.md, composer.json, and Instagram.php

--- src/Instagram.php
+++ src/Instagram.php
@@ -12,15 +12,25 @@ class Instagram
 
     public function fetch() 
     {
-        $html = self::makeRequest($this->username, $httpCode);
+        $html = self::makeRequest($this->username, array(), $httpCode);
 
         if ($httpCode < 200 || $httpCode >= 300) {
             throw new \Exception('Request failed. Code: '.$httpCode, 1);
             return false;
         }
 
+        if (! $this->isDataPresent($html)) {
+            // something went wrong, lets try again with some new headers
+            $html = self::makeRequest($this->username, $this->generateRequestHeaders(), $httpCode);  
+        }
+
+        if (! $this->isDataPresent($html)) {
+            throw new \Exception('Something went wrong', 1);
+            return false;
+        }
+
         if (! $json = $this->extractJson($html)) {
-            throw new \Exception('Unable to extract data', 1);            
+            throw new \Exception('Unable to extract data', 1);
             return false;
         }
 
@@ -68,16 +78,49 @@ class Instagram
         return $pictures;
     }
 
-    public static function makeRequest($userName, &$httpCode = '') 
+    public static function makeRequest($userName, $headers = array(), &$httpCode = '') 
     {
-        $ch = curl_init("https://www.instagram.com/$userName/");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        $content  = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $options = array(
+            CURLOPT_URL             => "https://www.instagram.com/$userName/", 
+            CURLOPT_SSL_VERIFYPEER  => false, // don't verify certificate
+            CURLOPT_RETURNTRANSFER  => true,  // return content
+            CURLOPT_FOLLOWLOCATION  => true   // follow redirects
+        );
+
+        if ($headers) {
+            $options[CURLOPT_HTTPHEADER] = $headers;
+        }        
+
+        //--------------
+
+        $ch         = curl_init();
+        curl_setopt_array($ch, $options);
+        $content    = curl_exec($ch);
+        $httpCode   = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
+
+        //--------------
+
         return $content;
+    }
+
+    protected function isDataPresent($string) 
+    {
+        return (bool) substr_count($string, '{"ProfilePage"');
+    }
+
+    protected function generateRequestHeaders() 
+    {
+        return array(
+            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+            $this->generateCookieHeader() 
+        );
+    }
+
+    protected function generateCookieHeader() 
+    {
+        $time = time();
+        return 'Cookie: sessionid='.$time.'; ds_user_id='.md5( $time );
     }
 
     /** remove duplicates */
